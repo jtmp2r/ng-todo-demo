2624a1814d56a842348324cca029da67d9448a12
Teriq Perry <jtmp2r@gmail.com>
2016-05-21 11:55:01 -0500

    search, repeat, etc.

--- app/app.js
+++ app/app.js
@@ -7,7 +7,44 @@ app.controller("NavCtrl", function($scope) {
 
 app.controller("TodoCtrl", function($scope) {
 	$scope.welcome = "Hello";
-	$scope.showListView = "true"
+	$scope.showListView = true;
+	$scope.newTask = {};
+
+	$scope.items = [
+		{
+			id: 0,
+			task: "mow the lawn, Teriq",
+			isCompleted: true,
+			dueDate: "12/5/17",
+			assignTo: 'greg',
+			location: "Zoe's House",
+			urgency: "low",
+			tools: 'sunshine, clippers, hat, water, headphones'
+		},
+		{
+			id: 1,
+			task: "grade quizzes",
+			isCompleted: true,
+			dueDate: "12/5/17",
+			assignTo: 'Joe',
+			location: "NSS",
+			urgency: "HIGH",
+			tools: 'wifi, tissues, vodka'
+		},
+		{
+			id: 2,
+			task: "Workout",
+			isCompleted: false,
+			dueDate: "12/5/17",
+			assignTo: 'Teriq',
+			location: "Anywhere",
+			urgency: "Mediun",
+			tools: "gym, body, fire"
+		}
+
+
+
+	]
 
 	$scope.newItem = function() {
 		console.log("click me BITCH!!!")
@@ -17,6 +54,13 @@ app.controller("TodoCtrl", function($scope) {
 		console.log("click me! click me! CLICK ME!!!!!")
 		$scope.showListView = true;
 
+	}
+
+	$scope.addNewItem= function() {
+		$scope.newTask.id = $scope.items.length;
+		console.log("hello... cunt", $scope.newTask)
+		$scope.items.push($scope.newTask);
+		$scope.newTask = "";
 	}
 
 })
