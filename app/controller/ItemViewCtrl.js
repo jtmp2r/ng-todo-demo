0aa31d95edaa563fea3a5a00eae26c6bdc9c4668
Teriq Perry <jtmp2r@gmail.com>
2016-05-23 20:56:17 -0500

    it's all in the details

--- app/controller/ItemViewCtrl.js
+++ app/controller/ItemViewCtrl.js
@@ -1,3 +1,30 @@
-app.controller("ItemViewCtrl", function($scope){
-    $scope.welcome = "hello";
+app.controller("ItemViewCtrl", function($scope, $http, $routeParams){
+	$scope.items = [];
+	$scope.selectedItem = {};
+	console.log($routeParams.itemId);
+
+
+	$http.get("https://angprac.firebaseio.com/items.json")
+			.success(function(itemObject) {
+				console.log("check", itemObject)
+				var itemCollection = itemObject;
+				console.log('itemObject', itemCollection)
+				Object.keys(itemCollection).forEach(function(key) {
+					itemCollection[key].id=key;
+					$scope.items.push(itemCollection[key]);
+
+					$scope.selectedItem = $scope.items.filter(function(item) {
+						return item.id === $routeParams.itemId;
+					})[0];
+				})
+			})
+
 });
+	
+
+
+
+	// $http.get(`https://angprac.firebaseio.com/items/${$routeParams.itemId}.json`)
+	// 	.success(function(response){
+	// 		console.log("fuck", response)
+	// 	})
